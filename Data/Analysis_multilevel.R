d44edb82d9cde766c88123ed31dca0ea910121d2
meerim <mekaruslanova@gmail.com>
2016-04-18 12:41:46 +0200

    added first 2 steps, however, we still have 15 groups (states)

--- Data/Analysis_multilevel.R
+++ Data/Analysis_multilevel.R
@@ -8,8 +8,6 @@ library(C)
 setwd('~/GitHub/Climate-Happiness/Data')
 source('~/GitHub/Climate-Happiness/Data/SourceFile.R')
 attach(finaldata)
-numdata <- transform(finaldata,Stateid=as.numeric(factor(State)))
-attach(numdata)
 Null.Model<-lme(satis~1,random=~1|Stateid,data=data,
                   control=list(opt="optim"))
 VarCorr(Null.Model)
@@ -24,3 +22,9 @@ logLik(Null.Model.2)*-2
 logLik(Null.Model)*-2
 sigtest <- (logLik(Null.Model.2)*-2)-(logLik(Null.Model)*-2) #variation is significant
 anova(Null.Model,Null.Model.2)
+
+Model.1<-lme(satis~environ+Emissions,random=~1|Stateid,data=data,
+             control=list(opt="optim"))
+summary(Model.1)
+
+
