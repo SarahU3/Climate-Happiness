92f2894285ffe51065ac8b1a9ad7ca00e693dc61
meerim <mekaruslanova@gmail.com>
2016-04-19 11:51:53 +0200

    models added

--- Data/Analysis_multilevel.R
+++ Data/Analysis_multilevel.R
@@ -2,7 +2,7 @@
 setwd('~/GitHub/Climate-Happiness/Data')
 source('~/GitHub/Climate-Happiness/Data/SourceFile.R')
 attach(finaldata)
-Null.Model<-lme(satis~1,random=~1|Stateid,data=data,
+Null.Model<-lme(satis~1,random=~1|Stateid, data=finaldata,
                   control=list(opt="optim"))
 VarCorr(Null.Model)
 
@@ -10,16 +10,16 @@ GREL.DAT<-GmeanRel(Null.Model)
 names(GREL.DAT)
 mean(GREL.DAT$MeanRel)
 
-Null.Model.2<-gls(satis~1,data=data,
+Null.Model.2<-gls(satis~1,data=finaldata,
                     control=list(opt="optim"))
 logLik(Null.Model.2)*-2
 logLik(Null.Model)*-2
 sigtest <- (logLik(Null.Model.2)*-2)-(logLik(Null.Model)*-2) #variation is significant
 anova(Null.Model,Null.Model.2)
 
-Model.1<-lme(satis~environ+Emissions+Use+Elec+Primary,random=~1|Stateid,data=data,
+Model.1<-lme(satis~environ+Emission,random=~1|Stateid,data=finaldata,
              control=list(opt="optim"))
 
 summary(Model.1)
-
+VarCorr(Model.1)
 
