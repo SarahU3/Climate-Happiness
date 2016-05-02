a7dd4a1b35755ddc07268cad89dc9dc0fb0e3ede
meerim <mekaruslanova@gmail.com>
2016-05-02 13:31:05 +0200

    interaction age, but interplot still not working

--- Data/Analysis_multilevel.R
+++ Data/Analysis_multilevel.R
@@ -1,8 +1,10 @@
 
 setwd('~/GitHub/Climate-Happiness/Data')
 source('~/GitHub/Climate-Happiness/Data/SourceFile.R')
-attach(finaldata)
-Null.Model<-lme(satis~1,random=~1|Stateid, data=finaldata,
+data <- read.csv("All_Merged_Data.csv")
+attach(data)
+
+Null.Model<-lme(satis~1,random=~1|Stateid, data=data,
                   control=list(opt="optim"))
 VarCorr(Null.Model)
 
@@ -10,14 +12,14 @@ GREL.DAT<-GmeanRel(Null.Model)
 names(GREL.DAT)
 x <- round(mean(GREL.DAT$MeanRel), digits=3)
 
-Null.Model.2<-gls(satis~1,data=finaldata,
+Null.Model.2<-gls(satis~1,data=data,
                     control=list(opt="optim"))
 logLik(Null.Model.2)*-2
 logLik(Null.Model)*-2
 sigtest <- (logLik(Null.Model.2)*-2)-(logLik(Null.Model)*-2) #variation is significant
 anova(Null.Model,Null.Model.2)
 
-Model.1<-lme(satis~environ+Emissions,random=~1|Stateid, data=finaldata,
+Model.1<-lme(satis~environ+Emissions,random=~1|Stateid, data=data,
              control=list(opt="optim"))
 
 summary(Model.1)
@@ -28,10 +30,10 @@ Model.2<-lme(satis~environ+Emissions+age+fam+gender+emp,random=~age+fam+gender+e
 
 summary(Model.2)
 
-Model.3<-lme(satis~environ+Emissions+Use+Primary,random=~1|Stateid,data=finaldata,
+Model.3<-lme(satis~environ+Emissions+Use+Primary,random=~1|Stateid,data=data,
              control=list(opt="optim"))
 
-Model.4<-lme(satis~environ+age+Emissions,random=~age|Stateid, data=finaldata,
+Model.4<-lme(satis~environ+age+Emissions,random=~age|Stateid, data=data,
              control=list(opt="optim"))
 
 summary(Model.4)
@@ -39,12 +41,17 @@ Model.4a<-update(Model.4,random=~1|Stateid)
 (anova(Model.4,Model.4a))$L.Ratio #L.Ratio is the sig test (significant)
 
 # Interaction
-Model.I<-lme(satis~environ+emp+age+fam+Emissions+fam*age,
-                   random=~1+emp+age+fam|Stateid,data=finaldata,control=list(opt="optim"))
+Model.I<-lme(satis~environ+emp+Emissions+fam*age,
+                   random=~1+emp+age+fam|Stateid,data=data,control=list(opt="optim"))
 
-round(summary(Model.I)$tTable,dig=3)
+Model.I2<-lme(satis~emp+fam+Emissions+environ*age,
+             random=~1+emp+fam+environ*age|Stateid,data=data,control=list(opt="optim"))
+interplot(m = Model.I2, var1 = "environ", var2 = "age")
+
+
+round(summary(Model.I2)$tTable,dig=3)
 
 m_fam<-lme(satis~fam*age,
-             random=~1+age+fam|Stateid,data=finaldata,control=list(opt="optim"))
+             random=~1+age*fam|Stateid,data=data,control=list(opt="optim"))
 library(interplot)
 interplot(m = m_fam, var1 = "fam", var2 = "age")
